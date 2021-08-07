b6c4bc1a750cf8bf0602cdc55e1eda6e8929ae3f
sharansajivmenon <sharansajivmenon@gmail.com>
2021-08-07 19:58:13 -0400

    Initial Commit

---
+++ MacTimer/MacTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacTimerApp.swift
+//  MacTimer
+//
+//  Created by Sharan Sajiv Menon on 8/7/21.
+//
+
+import SwiftUI
+
+@main
+struct MacTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
